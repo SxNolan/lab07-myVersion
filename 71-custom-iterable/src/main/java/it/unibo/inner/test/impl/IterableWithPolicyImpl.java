b703f65255e51888a52bbdd1579514502a8e015e
Christian Luca <Giovanni.luca@studio.unibo.it>
2023-11-06 11:33:15 +0100

    Terminato eserczio 71 (con un pò di aiuto sulla comprensione della classe Predicate). Pushing su repository remoto.

--- 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
@@ -1,6 +1,8 @@
 package it.unibo.inner.test.impl;
 
+import java.io.IOException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 
 import it.unibo.inner.api.IterableWithPolicy;
@@ -8,9 +10,30 @@ import it.unibo.inner.api.IterableWithPolicy;
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
     private T[] newArray;
+    Predicate<T> myPredicate;
 
     public IterableWithPolicyImpl(T[] myArray) {
+        this(myArray, new Predicate<T>() {
+            @Override
+            public boolean test(T elem) {
+                return true;
+            }
+        });
+    }
+
+    public IterableWithPolicyImpl(T[] myArray, Predicate<T> myPredicate) {
         this.newArray = myArray;
+        this.myPredicate = myPredicate;
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate<T> filter) {
+        this.myPredicate = filter;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyIteratorImpl();
     }
 
     class MyIteratorImpl implements Iterator<T>{
@@ -19,23 +42,29 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
         @Override
         public boolean hasNext() {
-            return index < IterableWithPolicyImpl.this.newArray.length;
+            while (this.index < newArray.length) {
+                T elem = newArray[this.index];
+                if (myPredicate.test(elem)) {
+                    return true;
+                }
+                this.index++;
+            }
+            return false;
         }
-    
+
         @Override
         public T next() {
-            return IterableWithPolicyImpl.this.newArray[index++];
+            if(hasNext()) {
+                T tempElem = newArray[this.index];
+                this.index++;
+                return tempElem;
+            }
+            throw new NoSuchElementException();
         }
     }
 
-    @Override
-    public Iterator<T> iterator() {
-        return new MyIteratorImpl();
-    }
+    
 
-    @Override
-    public void setIterationPolicy(Predicate<T> filter) {
-
-    }
+    
 
 }
